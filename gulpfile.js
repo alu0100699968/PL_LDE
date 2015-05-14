51083892d8991a8cfc02464cb4a8034800da4f8d
Juan Oliva <alu0100502923@ull.edu.es>
2015-05-14 18:51:17 +0100

    Empezando con karma y gulp #4

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp    = require('gulp'),
+gutil   = require('gulp-util'),
+uglify  = require('gulp-uglify'),
+concat  = require('gulp-concat');
+var del     = require('del');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
+var karma = require('karma').server;
+
+gulp.task('minify', function () {
+  gulp.src('./app.coffee')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./public/javascript/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'));
+
+  gulp.src('./views/*.jade')
+  .pipe(minifyHTML())
+  .pipe(gulp.dest('./minified/'))
+
+  gulp.src('./public/stylesheets/*.css')
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
+
+gulp.task('tests', function(done) {
+  return karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
+
+gulp.task('default', function(done) {
+  return karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
+});
